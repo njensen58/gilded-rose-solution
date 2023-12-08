8febfa6ef54140a27237973a83e0dc6583fcc312
Nathan Jensen <nate@nathans-macbook-pro.home>
2023-12-08 11:45:55 +0100

    ensure correct typing in service and remove redundancies

--- app/itemUpdaterService.ts
+++ app/itemUpdaterService.ts
@@ -1,58 +1,60 @@
+import { Item } from "./gilded-rose";
+
 export default class ItemUpdaterService {
 	private readonly minimum = 0;
 	private readonly maximum = 50;
 
-	public brie(brie): void {
+	public brie(brie: Item): void {
 		if (brie.quality < this.maximum) {
 			if (brie.sellIn > 0) {
-				brie.quality += 1
+				brie.quality += 1;
 			} else {
-				brie.quality = this.setMinQuality(brie.quality + 2, this.maximum);
+				brie.quality = this.setMinQuality(brie.quality + 2);
 			}
 		}
-		this.decrementSellIn(brie)
+		this.decrementSellIn(brie);
 	}
 
-	public backstagePasses(pass): void {
+	public backstagePasses(pass: Item): void {
 		if (pass.sellIn > 10) {
-			pass.quality = this.setMinQuality(pass.quality + 1, this.maximum);
+			pass.quality = this.setMinQuality(pass.quality + 1);
 		} else if (pass.sellIn > 5) {
-			pass.quality = this.setMinQuality(pass.quality + 2, this.maximum);
+			pass.quality = this.setMinQuality(pass.quality + 2);
 		} else if (pass.sellIn > 0) {
-			pass.quality = this.setMinQuality(pass.quality + 3, this.maximum);
+			pass.quality = this.setMinQuality(pass.quality + 3);
 		} else {
 			pass.quality = 0;
 		}
-		this.decrementSellIn(pass)
+		this.decrementSellIn(pass);
 	}
 
-	public standardItem(item): void {
+	public standardItem(item: Item): void {
 		if (item.sellIn > this.minimum) {
-			item.quality = this.setMaxQuality(item.quality - 1, this.minimum);
+			item.quality = this.setMaxQuality(item.quality - 1);
 		} else {
-			item.quality = this.setMaxQuality(item.quality - 2, this.minimum);
+			item.quality = this.setMaxQuality(item.quality - 2);
 		}
-		this.decrementSellIn(item)
+		this.decrementSellIn(item);
 	}
 
-	public conjuredItem(item): void {
+	public conjuredItem(item: Item): void {
 		if (item.sellIn > this.minimum) {
-			item.quality = this.setMaxQuality(item.quality - 2, this.minimum);
+			item.quality = this.setMaxQuality(item.quality - 2);
 		} else {
-			item.quality = this.setMaxQuality(item.quality - 4, this.minimum);
+			item.quality = this.setMaxQuality(item.quality - 4,);
 		}
-		this.decrementSellIn(item)
+		this.decrementSellIn(item);
 	}
 
-	private setMaxQuality(newQuality, max) {
-		return Math.max(newQuality, max);
+	private setMaxQuality(newQuality: number): number {
+		return Math.max(newQuality, this.minimum);
 	}
 
-	private setMinQuality(newQuality, min) {
-		return Math.min(newQuality, min);
+	private setMinQuality(newQuality: number): number {
+		return Math.min(newQuality, this.maximum);
 	}
 
-	private decrementSellIn(item): void {
+	private decrementSellIn(item: Item): void {
 		item.sellIn -= 1;
 	}
 }
