a3921f88df5bc75e76cdc3feec1c492d36edb219
Nathan Jensen <nate@nathans-macbook-pro.home>
2023-12-08 10:59:35 +0100

    tests for integration with itemUpdaterService

--- test/jest/gilded-rose.spec.ts
+++ test/jest/gilded-rose.spec.ts
@@ -1,6 +1,41 @@
 import { Item, GildedRose } from '@/gilded-rose';
 import { AGED_BRIE, SULFURAS, BACKSTAGE_PASSES, CONJURED } from '@/constants';
+import ItemUpdaterService from '../../app/itemUpdaterService';
 
+describe("GildedRose class", () => {
+  const service = new ItemUpdaterService();
+  const brieSpy = jest.spyOn(service, 'brie');
+  const backstageSpy = jest.spyOn(service, 'backstagePasses');
+  const conjuredSpy = jest.spyOn(service, 'conjuredItem');
+  const standardSpy = jest.spyOn(service, 'standardItem');
+
+  it("Should call the updater service with a given item", () => {
+    const brie = new Item(AGED_BRIE, 10, 50);
+    const sulfuras = new Item(SULFURAS, -1, 80);
+    const passes = new Item(BACKSTAGE_PASSES, 5, 50);
+    const conjured = new Item(CONJURED, 10, 50);
+    const standard = new Item("Golden Gun", 10, 15);
+
+    const gildedRose = new GildedRose(
+      [brie, sulfuras, passes, conjured, standard],
+      service
+    );
+
+    gildedRose.updateQuality();
+
+    expect(brieSpy).toHaveBeenCalled();
+    expect(brieSpy).toHaveBeenCalledWith(brie);
+
+    expect(backstageSpy).toHaveBeenCalled();
+    expect(backstageSpy).toHaveBeenCalledWith(passes)
+
+    expect(conjuredSpy).toHaveBeenCalled();
+    expect(conjuredSpy).toHaveBeenCalledWith(conjured)
+
+    expect(standardSpy).toHaveBeenCalled();
+    expect(standardSpy).toHaveBeenCalledWith(standard)
+  });
+});
 
 describe(AGED_BRIE, () => {
   it("should never have a quality > 50", () => {
